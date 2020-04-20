53b846266cefc485b378078f22e4ac5b60b9994f
Mark Ghatas <marksamyhanna@gmail.com>
2020-04-19 22:39:28 -0500

    removing dist

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -3,6 +3,7 @@ import { UserService } from 'src/app/services/user.service';
 import { ContextService } from 'src/app/services/context.service';
 import { User } from 'src/app/models/user';
 import { Router } from '@angular/router';
+import { roleEnum } from '../../models/roles';
 
 @Component({
   selector: 'app-profile',
@@ -19,6 +20,9 @@ export class ProfileComponent implements OnInit {
     private router: Router) { }
 
   ngOnInit(): void {
+    this.user = new User("", "", "", "", "", new Date("2020-01-16") ,new Array<String>(), false, 0, new Array<User>(),
+    roleEnum.user, ''); 
+
     this.userService.getUser(this.contextService.getUserId()).subscribe(
       response => {
         this.user = response;
@@ -32,6 +36,10 @@ export class ProfileComponent implements OnInit {
         console.log(<any>error);
       }
     );
+  }
+
+  formatDate(date: Date) {
+    return date.toString().split('T', 1);
   }
 
   onHomeClick() {
